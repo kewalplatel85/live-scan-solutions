5c7cd8315f235650abee544edde87ca80208c4f7
Pritesh Patel <pritesh.patel52@outlook.com>
2025-07-20 23:48:35 -0700

    fix hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,11 @@ import { FAQSection } from '@/components/sections/FAQSection';
 import { HeroSection } from '@/components/sections/HeroSection';
 import { HowWorksSection } from '@/components/sections/HowWorksSection';
 import { ServiceTypesSection } from '@/components/sections/ServiceTypesSection';
-import { TrustSection } from '@/components/sections/TrustSection';
 
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
-      <TrustSection />
       <ServiceTypesSection />
       <AboutSection />
       <CustomerTypesSection />
