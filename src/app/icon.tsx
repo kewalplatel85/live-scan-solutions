afd30f109a8f3632c6d83a50ac96af50ef7ea1cc
Pritesh Patel <pritesh.patel52@outlook.com>
2025-07-27 17:09:06 -0700

    fix the build

--- src/app/icon.tsx
+++
@@ -1,3 +0,0 @@
-// This file is not needed since we're using the PNG images directly
-// The favicon will use /assets/Logo/icons8-fingerprint-scan-48.png
-export { default } from '/public/assets/Logo/icons8-fingerprint-scan-48.png';
